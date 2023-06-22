f4d5191d218d17f9e26e69eb2a089bad0ab2f9d3
Sergei Kharitontcev-Beglov <khbminus@gmail.com>
2023-06-22 14:45:26 +0200

    changed dataSource

--- src/diff/commonDiffResources.ts
+++ src/diff/commonDiffResources.ts
@@ -1,18 +1,18 @@
-import {kotlinDeclarationsSize as sizes1} from "../ir-sizes-stdlib";
-import {kotlinDeclarationsSize as sizes2} from "../ir-sizes-stdlib-changed";
+import {kotlinDeclarationsSize as sizes1} from "../ir-sizes";
+import {kotlinDeclarationsSize as sizes2} from "../ir-sizes-changed";
 
-import {kotlinRetainedSize as rsz1} from "../ir-sizes-stdlib-retained";
-import {kotlinRetainedSize as rsz2} from "../ir-sizes-stdlib-changed-retained";
+import {kotlinRetainedSize as rsz1} from "../ir-sizes-retained";
+import {kotlinRetainedSize as rsz2} from "../ir-sizes-changed-retained";
 
-import {diffDeclarationsSizes as dd} from "./stdlib-diff/ir-sizes";
-import {diffReachibilityInfos as dr} from "./stdlib-diff/dce-graph";
-import {diffMetaNodesInfo as dm} from "./stdlib-diff/metanodes";
-import {diffDeclarationsDifference as ddd} from "./stdlib-diff/node-diff";
+import {diffDeclarationsSizes as dd} from "./simple-diff/ir-sizes";
+import {diffReachibilityInfos as dr} from "./simple-diff/dce-graph";
+import {diffMetaNodesInfo as dm} from "./simple-diff/metanodes";
+import {diffDeclarationsDifference as ddd} from "./simple-diff/node-diff";
 
-import {diffDeclarationsSizes as rdd} from "./stdlib-diff-retained/ir-sizes";
-import {diffReachibilityInfos as rdr} from "./stdlib-diff-retained/dce-graph";
-import {diffMetaNodesInfo as rdm} from "./stdlib-diff-retained/metanodes";
-import {diffDeclarationsDifference as rddd} from "./stdlib-diff-retained/node-diff";
+import {diffDeclarationsSizes as rdd} from "./simple-diff-retained/ir-sizes";
+import {diffReachibilityInfos as rdr} from "./simple-diff-retained/dce-graph";
+import {diffMetaNodesInfo as rdm} from "./simple-diff-retained/metanodes";
+import {diffDeclarationsDifference as rddd} from "./simple-diff-retained/node-diff";
 
 
 export const irMap1 = new Map(Object.entries(sizes1).map(x => [x[0], x[1].size]));
--- src/graph/graph1.ts
+++ src/graph/graph1.ts
@@ -1,5 +1,5 @@
 import {draw} from "./graphCommon";
-import {kotlinDeclarationsSize} from "../ir-sizes1";
+import {kotlinDeclarationsSize} from "../ir-sizes-changed";
 import {kotlinReachabilityInfos} from "../dce-graph1";
 import {kotlinRetainedSize} from "../retained-size1";
 
